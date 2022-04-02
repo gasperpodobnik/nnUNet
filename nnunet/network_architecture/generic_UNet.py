866bcdeaca79c1d28dcd1a41ef2ea9392ff64657
gasperp <gasper.podobnik@gmail.com>
2022-04-02 18:06:53 +0000

    equal number of feature maps in each path commit

--- nnunet/network_architecture/generic_UNet.py
+++ nnunet/network_architecture/generic_UNet.py
@@ -291,14 +291,14 @@ class Generic_UNet(SegmentationNetwork):
             if d < self.merge_after_n_blocks:
                 print('\n\nUsing dual path config' + f'num channels: {input_channels}' + '\n\n')
                 # separate paths at encoding part
-                # factor = 2
-                # path1_input_features = path2_input_features = int(input_features/factor)
-                # path1_output_features = path2_output_features = int(output_features/factor)
+                factor = 2
+                path1_input_features = path2_input_features = int(input_features/factor)
+                path1_output_features = path2_output_features = int(output_features/factor)
                 
-                path1_input_features = input_features - 1
-                path2_input_features = 1
-                path1_output_features = output_features - 1
-                path2_output_features = 1
+                # path1_input_features = input_features - 1
+                # path2_input_features = 1
+                # path1_output_features = output_features - 1
+                # path2_output_features = 1
                 print(f'Path1 in: {path1_input_features}, out: {path1_output_features}')
                 print(f'Path2 in: {path2_input_features}, out: {path2_output_features}')
                 self.conv_blocks_context_path1.append(StackedConvLayers(path1_input_features, path1_output_features, num_conv_per_stage,
